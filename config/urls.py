1f743606318a890f8155a11738a13f058a5a737f
elena <kev7570@gmail.com>
2026-07-26 14:35:22 +0300

    Complete Task 1: JWT authentication, token endpoints, user registration

--- config/urls.py
+++ config/urls.py
@@ -19,11 +19,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('lms.urls')),
     path('api/', include('users.urls')),
+
+    # JWT endpoints
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 if settings.DEBUG:
--- users/views.py
+++ users/views.py
@@ -1,8 +1,9 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter
 from .models import Payment
-from .serializers import PaymentSerializer
+from .serializers import PaymentSerializer, UserRegistrationSerializer, UserSerializer
+from rest_framework.response import Response
 
 
 class PaymentListView(generics.ListAPIView):
@@ -12,4 +13,17 @@ class PaymentListView(generics.ListAPIView):
     filterset_fields = ['course', 'lesson', 'payment_method']
     ordering_fields = ['payment_date']
     ordering = ['-payment_date']  # По умолчанию сортировка по убыванию
+
+class UserRegistrationView(generics.CreateAPIView):
+    serializer_class = UserRegistrationSerializer
+    permission_classes = [permissions.AllowAny]  # Доступно всем
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({
+            'user': UserSerializer(user).data,
+            'message': 'Пользователь успешно создан'
+        })
     
